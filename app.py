ecc32dc1536402c8f33a62b81abd5c96336730cf
van1llaface <arnoldaszeimis@gmail.com>
2025-01-23 20:54:14 +0200

    only hello world left

--- app.py
+++ app.py
@@ -6,9 +6,3 @@ and includes a function with proper linting.
 def main():
     """Main function to execute the program."""
     print("Hello, World!")
-
-    if 5 > 2:
-        print("5 is greater than 2")
-
-if __name__ == "__main__":
-    main()
